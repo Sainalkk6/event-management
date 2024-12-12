61a1d73a8183a0edcfa25ceafe3f1251c761962f
sainal <sainalkk6@gmail.com>
2024-12-12 18:33:34 +0530

    fix:added error handler for bookings fetching

--- src/app/api/booking/get-bookings/route.ts
+++ src/app/api/booking/get-bookings/route.ts
@@ -8,11 +8,15 @@ export const GET = async (req: NextRequest) => {
     const userEmail = session?.user?.email
     
     try {
-        const eventIds = await prisma.bookings.findMany({ where: { userId: userEmail ?? "" } })
+        if(userEmail){
+            const eventIds = await prisma.bookings.findMany({ where: { userId: userEmail ?? "" } })
         const events = await Promise.all(
             eventIds.map(async (event) => await prisma.event.findMany({ where: { id: event.eventId } }))
         )
         return NextResponse.json(events)
+        } else{
+            return NextResponse.json({message:"Something went wrong while fetching the events for the current user"})
+        }
     } catch (err) {
         return NextResponse.json({ message: err })
     }
